551efeab6f78777b4759bff10a689d636bb7a0c7
Jay <quocphong.luong@my.jcu.edu.au>
2020-04-16 12:31:15 +1000

    changing numbers on list

---
+++ lists_warmup.py
@@ -0,0 +1,11 @@
+numbers = [3,1,4,1,5,9,2]
+# Changge the first element of numbers to "ten"
+numbers[0] = "ten"
+print(numbers)
+# Change the last element of number to 1
+numbers[-1] = 1
+print(numbers)
+# Get all the elements from numbers except the first two
+print(numbers[2:])
+# Check if 9 is an element of numbers
+print(9 in numbers)
