a82b9c40241b0b4d08c49333b681306e63e3f7ea
Jay <quocphong.luong@my.jcu.edu.au>
2020-04-17 14:10:44 +1000

    cumulative total calculation

--- prac_4/total_income.py
+++ prac_4/total_income.py
@@ -20,6 +20,7 @@ def print_report(incomes, number_of_months):
     """print report based on incomes"""
     print("\nIncome Report\n-------------")
     total = 0
+    
     for month in range(1, number_of_months + 1):
         income = incomes[month - 1]
         total += income
