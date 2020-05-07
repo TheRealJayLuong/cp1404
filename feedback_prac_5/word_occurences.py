0edc0222f503413365b8f50d982a9daca1955358
Jay <quocphong.luong@my.jcu.edu.au>
2020-05-07 14:51:43 +1000

    Use of dictionary and apply string formatting

---
+++ feedback_prac_5/word_occurences.py
@@ -0,0 +1,16 @@
+"""Count the occurrences of words in a string"""
+counts = {}
+text = input("Please enter your Text:")
+texts = text.split()
+
+
+for word in texts:
+    counts[word] = counts.get(word,0) + 1
+
+texts = list(counts.keys())
+texts.sort()
+
+max_length = max((len(word)for word in texts))
+
+for word in texts:
+    print("{:{}} : {}".format(word, max_length, counts[word]))
