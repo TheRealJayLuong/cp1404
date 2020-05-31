80db0b0c556dc6fc0d948167b853bd2e44d74d29
Jay <quocphong.luong@my.jcu.edu.au>
2020-05-31 22:36:03 +1000

    modify upper case for first letter

--- prac_9/cleanup_files.py
+++ prac_9/cleanup_files.py
@@ -1,66 +1,39 @@
-import shutil
 import os
 
 
 def main():
-    """Demo os module functions."""
-    print("Starting directory is: {}".format(os.getcwd()))
+    """Cleanup inconsistent song lyrics file names."""
 
     # Change to desired directory
-    os.chdir('Lyrics/Christmas')
-
-    # Print a list of all files in current directory
-    print("Files in {}:\n{}\n".format(os.getcwd(), os.listdir('.')))
-
-    # Make a new directory
-    # The next time you run this, it will crash if the directory exists
-
-    try:
-        os.mkdir('temp')
-    except FileExistsError:
-        pass
-
-    # Loop through each file in the (current) directory
-    for filename in os.listdir('.'):
-        # Ignore directories, just process files
-        if os.path.isdir(filename):
-            continue
-
-        new_name = get_fixed_filename(filename)
-        print("Renaming {} to {}".format(filename, new_name))
-
-        # Option 1: rename file to new name - in place
-        # os.rename(filename, new_name)
-    os.rename(r'C:\Users\phong\Desktop\python\cp1404\prac_9\Lyrics\Special\Remember The Lord.txt',
-              r'C:\Users\phong\Desktop\python\cp1404\prac_9\Lyrics\Special\Remember The Lord Of The Ring .txt')
-    # Option 2: move file to new place, with new name
-    # shutil.move(filename, 'temp/' + new_name)
-
-    shutil.move(r'C:\Users\phong\Desktop\python\cp1404\prac_9\Lyrics\Special\The Lord is King.txt',
-                'temp/King is The Lord.txt')
-
-
-def get_fixed_filename(filename):
-    """Return a 'fixed' version of filename."""
-    new_name = filename.replace(" ", "_").replace(".TXT", ".txt")
-    return new_name
-
-
-def demo_walk():
-    """Process all subdirectories using os.walk()."""
-    os.chdir(r'C:\Users\phong\Desktop\python\cp1404\prac_9\Lyrics')
+    os.chdir('Lyrics')
     for directory_name, subdirectories, filenames in os.walk('.'):
         print("Directory:", directory_name)
         print("\tcontains subdirectories:", subdirectories)
         print("\tand files:", filenames)
         print("(Current working directory is: {})".format(os.getcwd()))
 
-        # display a loop to rename the files
         for filename in filenames:
+            new_name = get_fixed_filename(filename)
+
+            print("Renaming {} to {}".format(filename, new_name))
+
             full_name = os.path.join(directory_name, filename)
-            new_name = os.path.join(directory_name, get_fixed_filename(filename))
+            new_name = os.path.join(directory_name, new_name)
             os.rename(full_name, new_name)
+
+        for file in filenames:
+            if file[1].isalpha():
+                os.rename(file, file[:1] + file[0].upper())
+            print("Renamed " + file + "to" + file[:1] + file[0].upper())
+
+def get_fixed_filename(filename):
+    """Return a 'fixed' version of filename."""
+
+
+    new_name = filename.replace(" ", "_").replace(".TXT", ".txt")
+    return new_name
+
+
 
 
 main()
-demo_walk()
